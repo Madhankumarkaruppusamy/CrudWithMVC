7ad2decc8e19d1eb5ec78b28cc8c04b406c7469c
Madhan kumar K <madhankumarkaruppusamy@mail.com>
2023-12-25 14:39:56 +0530

    ModifiedMVC

---
+++ DataAccessLayer/CricketerRepository.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Dapper;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DapperDataAccessLayer
+{
+    public class CricketerRepository : ICricketerRepository
+    {
+
+        string connectionString = "Data source=DESKTOP-BLBGEHJ\\SQLEXPRESS;initial catalog=batch7;user id=sa;password=Anaiyaan@123;";
+        public void InsertSP(Cricketer details)
+        {
+            try
+            {
+                var con = new SqlConnection(connectionString);
+                con.Open();
+                var insertQuery = $"exec InsertSP @CricketerName='{details.CricketerName}', @TotalODI={details.TotalODI}, @TotalScore={details.TotalScore}, @Fifties={details.Fifties},@Hundreds={details.Hundreds} ";
+                con.Execute(insertQuery);
+                con.Close();
+
+
+            }
+            catch (SqlException sql)
+            {
+                throw;
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+
+        public List<Cricketer> ReadSP()
+        {
+            try
+            {
+                var con = new SqlConnection(connectionString);
+                con.Open();
+                var selectQuery = $"exec ReadSP";
+                var stats = con.Query<Cricketer>(selectQuery);
+
+                con.Close();
+                return stats.ToList();
+
+
+            }
+            catch (SqlException sql)
+            {
+                throw;
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+
+        public void DeleteSP(long CricketerId)
+        {
+            try
+            {
+
+                var con = new SqlConnection(connectionString);
+                con.Open();
+                var deleteQuery = $"exec DeleteSP {CricketerId}";
+                con.Execute(deleteQuery);
+                con.Close();
+
+            }
+            catch (SqlException sql)
+            {
+                throw;
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+        public void UpdateSP(long cricketerId, Cricketer prd)
+        {
+            try
+            {
+                var con = new SqlConnection(connectionString);
+                con.Open();
+                var updateQuery = $"exec UpdateSP  {cricketerId},'{prd.CricketerName}',{prd.TotalODI},{prd.TotalScore},{prd.Fifties},{prd.Hundreds} ";
+                var product = con.Query<Cricketer>(updateQuery);
+                con.Execute(updateQuery);
+                con.Close();
+
+
+            }
+            catch (SqlException sql)
+            {
+                throw;
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+    }
+}
+
+
---
+++ MVCMadhan/Controllers/CricketerDetailsController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DapperDataAccessLayer;
+
+namespace MVCMadhan.Controllers
+{
+    public class CricketerDetailsController : Controller
+    {
+        private readonly ICricketerRepository _obje;
+
+        public CricketerDetailsController()
+        {
+            _obje = new CricketerRepository();
+        }
+        // GET: CricketerDetailsController
+        public ActionResult Index()
+        {
+            var result = _obje.ReadSP();
+            return View("View",result);
+        }
+
+        // GET: CricketerDetailsController/Details/5
+        public ActionResult Details(int id)
+        {
+            var result = _obje.ReadSPById(id);
+            return View("Details",result);
+        }
+
+        // GET: CricketerDetailsController/Create
+        public ActionResult Create()
+        {
+            return View("Create",new Cricketer ());
+        }
+
+        // POST: CricketerDetailsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Cricketer pro)
+        {
+            try
+            {
+                _obje.InsertSP(pro);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CricketerDetailsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var result = _obje.ReadSPById(id);
+            return View("Edit",result);
+        }
+
+        // POST: CricketerDetailsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Cricketer pro)
+        {
+            try
+            {
+                _obje.Update(id, pro);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CricketerDetailsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var result = _obje.ReadSPById(id);
+            return View("Delete",result);
+        }
+
+        // POST: CricketerDetailsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteById(int id)
+        {
+            try
+            {
+                _obje.DeleteSPById(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
