3caf3d00771ab8fa439df96370c8f0cad486aeea
Madhan kumar K <madhankumarkaruppusamy@mail.com>
2024-01-16 20:08:14 +0530

    MVCModified

--- EntityFrameworkMVC/Registration.cs
+++ EntityFrameworkMVC/Registration.cs
@@ -11,6 +11,7 @@ namespace EntityFrameworkMVC
     {
         [Key]
         public long RegistrationId { get; set; }
+        [Required (ErrorMessage ="Enter Your EmailID")]
         public string Username { get; set; } 
 
         public string Password { get; set; }
--- MVCMadhan/Controllers/LoginController.cs
+++ MVCMadhan/Controllers/LoginController.cs
@@ -1,5 +1,8 @@
 ﻿using DapperDataAccessLayer;
+using EntityFrameworkMVC;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +11,127 @@ using System.Threading.Tasks;
 namespace MVCMadhan.Controllers
 {
     public class LoginController : Controller
-    {
-        public IActionResult Index()
+    { 
+        private readonly IRegistrationRepository _reg;
+        private readonly string Connection;
+        public LoginController(IRegistrationRepository Obj, IConfiguration configuration)
+        {
+            _reg = Obj;
+            Connection = configuration.GetConnectionString("Dbconnection");
+        }
+        // GET: LoginController
+        public ActionResult Index()
         {
             return View("Login");
         }
+
+        // GET: LoginController/Details/5
+        public ActionResult Details(long id)
+        {
+            var data = _reg.GetByid(id);
+            return View("View", data);
+        }
+
+        // GET: LoginController/Create
+        public ActionResult Create(Registration Obj)
+        {
+            try
+            {
+                var Get = _reg.Register(Obj);
+                if (Get == true)
+                {
+                    var get = _reg.GetAllRegistration();
+                    return View("View", get);
+
+                }
+                else
+                {
+                    return View("Login");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        // POST: LoginController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LoginController/Edit/5
+        public ActionResult Edit(Registration id)
+
+        {
+            var data = _reg.GetAllRegistration();
+            return View("Edit", data);
+        }
+
+        // POST: LoginController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LoginController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var send = _reg.GetByid(id);
+            return View("Delete", send);
+        }
+
+        // POST: LoginController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        /*public ActionResult Authentication(Login log)
+        {
+            try
+            {
+                var result = _reg.Login();
+                if (result = true)
+                {
+                    return Redirect("/Home/Index");
+
+                }
+                else
+                {
+                    ModelState.AddModelError("Password", "Invalid EmailID and Password");
+                    return View("Login");
+                }
+            
+            }
+        }*/
     }
 }
